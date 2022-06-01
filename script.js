589474947d410afcae3d57ac2a20e9d827e5e647
Kelly McGuffog <kellymcguffog@gmail.com>
2022-05-31 23:20:50 -0400

    beginning stages of javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+const clickButtonLeft = document.getElementById('buttonLeft');
+
+
+clickButtonLeft.addEventListener('click', function(){
+    document.getElementById('background').innerHTML="I am a Graphic Designer on the Corporate Affairs & Communications team at L'Oréal. I started working there after I graduated from Washington University in St. Louis in 2018.";
+});
+
+const clickButtonRight = document.getElementById('buttonRight');
+
+clickButtonRight.addEventListener('click', function(){
+    document.getElementById('background').innerHTML="hi";
+});
